c6838633de5b3fd5a31120936493eaec9cf8746b
Leonardo Luz Almeida <leonardo_almeida@intuit.com>
2024-10-07 21:28:24 -0400

    remove comments
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>

--- cmd/main.go
+++ cmd/main.go
@@ -9,17 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// const (
-// 	commandNameEnv = "EPHEMERAL_COMMAND_NAME"
-// 	cliName = "ephemeral-access"
-// )
-
 func main() {
-	// commandName := filepath.Base(os.Args[0])
-	// if val := os.Getenv(commandNameEnv); val != "" {
-	// 	commandName = val
-	// }
-
 	command := &cobra.Command{
 		Use:   "ephemeral-access",
 		Short: "Ephemeral Access command entrypoint",
@@ -32,16 +22,6 @@ func main() {
 
 	command.AddCommand(backend.NewCommand())
 	command.AddCommand(controller.NewCommand())
-
-	// switch commandName {
-	// case backend.CommandName:
-	// 	command = backend.NewCommand()
-	// case controller.CommandName:
-	// 	command = controller.NewCommand()
-	// default:
-	// 	fmt.Fprintf(os.Stderr, "invalid command name: %s\n", commandName)
-	// 	os.Exit(1)
-	// }
 
 	if err := command.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
