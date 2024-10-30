ce626af6150d119d33f92494b8b84c3394b78696
ashutosh16 <11219262+ashutosh16@users.noreply.github.com>
2024-10-30 12:58:42 -0700

    feat(ui): reset the widget when access expires
    Signed-off-by: ashutosh16 <11219262+ashutosh16@users.noreply.github.com>

--- ui/src/shared/context.ts
+++
@@ -1,9 +0,0 @@
-import {History} from 'history';
-import * as React from 'react';
-
-
-export interface ContextApis {
-  baseHref: string;
-}
-export const Context = React.createContext<ContextApis & {history: History}>(null);
-export const {Provider, Consumer} = Context;
