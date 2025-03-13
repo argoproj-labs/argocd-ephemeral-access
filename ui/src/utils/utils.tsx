d10c57fd97b4cf22a4ba93de0b3840c320313b41
asingh51 <11219262+ashutosh16@users.noreply.github.com>
2025-03-13 16:48:25 -0700

    fix(ui): empty value
    Signed-off-by: asingh51 <11219262+ashutosh16@users.noreply.github.com>

--- ui/src/utils/utils.tsx
+++ ui/src/utils/utils.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Application } from '../models/type';
-import {
-    AllowedRoleResponseBody,
-    listAllowedroles,
-} from '../gen/ephemeral-access-api';
-import {getHeaders} from "../config/client";
+import { AllowedRoleResponseBody, listAllowedroles } from '../gen/ephemeral-access-api';
+import { getHeaders } from '../config/client';
 
 export enum PermissionRole {
   DEFAULT_DISPLAY_ACCESS = 'Read',
   PERMISSION_REQUEST = 'PERMISSION REQUEST',
-  REQUEST_ROLE_LABEL = 'REQUEST ROLE',
+  REQUEST_ROLE_LABEL = 'REQUEST ROLE'
 }
 
 export const Spinner = ({ show, style = {} }: { show: boolean; style?: React.CSSProperties }) =>
@@ -19,14 +16,19 @@ export const Spinner = ({ show, style = {} }: { show: boolean; style?: React.CSS
     </span>
   ) : null;
 
-
 export const getDefaultDisplayAccessRole = (): string => {
-  return window?.EPHEMERAL_ACCESS_VARS?.EPHEMERAL_ACCESS_DEFAULT_DISPLAY_ACCESS || PermissionRole.DEFAULT_DISPLAY_ACCESS;
+  return (
+    window?.EPHEMERAL_ACCESS_VARS?.EPHEMERAL_ACCESS_DEFAULT_DISPLAY_ACCESS ||
+    PermissionRole.DEFAULT_DISPLAY_ACCESS
+  );
 };
 
 export const EnableEphemeralAccess = (application: Application) => {
-  if (window?.EPHEMERAL_ACCESS_VARS === undefined || window?.EPHEMERAL_ACCESS_VARS?.EPHEMERAL_ACCESS_LABEL_KEY === undefined
-    || window?.EPHEMERAL_ACCESS_VARS?.EPHEMERAL_ACCESS_LABEL_VALUE === undefined) {
+  if (
+    window?.EPHEMERAL_ACCESS_VARS === undefined ||
+    window?.EPHEMERAL_ACCESS_VARS?.EPHEMERAL_ACCESS_LABEL_KEY === undefined ||
+    window?.EPHEMERAL_ACCESS_VARS?.EPHEMERAL_ACCESS_LABEL_VALUE === undefined
+  ) {
     return true;
   }
 
@@ -39,43 +41,45 @@ export const EnableEphemeralAccess = (application: Application) => {
 };
 
 export const getAccessRoles = async (
-    applicationName: string,
-    applicationNamespace: string,
-    project: string,
-    username: string,
+  applicationName: string,
+  applicationNamespace: string,
+  project: string,
+  username: string
 ): Promise<AllowedRoleResponseBody[]> => {
-    const defaultRole = window?.EPHEMERAL_ACCESS_VARS?.EPHEMERAL_ACCESS_DEFAULT_TARGET_ROLE;
-    const defaultDisplayRole = window?.EPHEMERAL_ACCESS_VARS?.EPHEMERAL_ACCESS_DEFAULT_DISPLAY_ACCESS;
-    if (defaultRole) {
-        return [{ roleName: defaultRole, roleDisplayName: defaultDisplayRole}] as unknown as AllowedRoleResponseBody[];
-    } else {
-        try {
-            const response = await listAllowedroles({
-                headers: getHeaders({ applicationName, applicationNamespace, project, username })
-            });
-            return response.data.items;
-        } catch (error) {
-            throw new Error(`Failed to get allowed roles: ${error}`);
-        }
+  const defaultRole = window?.EPHEMERAL_ACCESS_VARS?.EPHEMERAL_ACCESS_DEFAULT_TARGET_ROLE;
+  const defaultDisplayRole = window?.EPHEMERAL_ACCESS_VARS?.EPHEMERAL_ACCESS_DEFAULT_DISPLAY_ACCESS;
+  if (defaultRole) {
+    return [
+      { roleName: defaultRole, roleDisplayName: defaultDisplayRole }
+    ] as unknown as AllowedRoleResponseBody[];
+  } else {
+    try {
+      const response = await listAllowedroles({
+        headers: getHeaders({ applicationName, applicationNamespace, project, username })
+      });
+      return response.data.items;
+    } catch (error) {
+      throw new Error(`Failed to get allowed roles: ${error}`);
     }
+  }
 };
 
 export function getDisplayTime(requestedAt: string | null | undefined): string {
   if (!requestedAt) {
-    return "";
+    return '';
   }
 
   const date = new Date(requestedAt);
   if (isNaN(date.getTime())) {
-    return "";
+    return '';
   }
 
   return date.toLocaleString();
 }
 
-export  function getDisplayValue(value: string) {
+export function getDisplayValue(value: string) {
   if (value === undefined) {
     return '';
   }
-  return value.toLowerCase()
+  return value.toLowerCase();
 }
