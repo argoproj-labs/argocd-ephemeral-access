102a3c4abf42cc48d97594c9c8c3ec0829b719a5
Peter Jiang <peterjiang823@gmail.com>
2025-04-01 11:28:35 -0700

    Add plugin metrics
    Signed-off-by: Peter Jiang <peterjiang823@gmail.com>

--- internal/controller/metrics/metrics.go
+++ internal/controller/metrics/metrics.go
@@ -1,6 +1,7 @@
 package metrics
 
 import (
+	"github.com/argoproj-labs/argocd-ephemeral-access/pkg/plugin"
 	"github.com/prometheus/client_golang/prometheus"
 	"sigs.k8s.io/controller-runtime/pkg/metrics"
 )
@@ -13,13 +14,40 @@ var (
 		},
 		[]string{"accessRequestStatus"},
 	)
+
+	// PluginOperationsTotal counts the total number of plugin operations. The plugin operation can be either
+	// revoke_access or grant_access.
+	pluginOperationsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "plugin_operations_total",
+			Help: "Total number of plugin operations by type and result",
+		},
+		[]string{"operation", "result"},
+	)
 )
 
 func init() {
 	metrics.Registry.MustRegister(accessRequestStatusTotal)
+	metrics.Registry.MustRegister(pluginOperationsTotal)
 }
 
 // IncrementAccessRequestCounter increments the counter for a given AccessRequest status
 func IncrementAccessRequestCounter(status string) {
 	accessRequestStatusTotal.WithLabelValues(status).Inc()
+}
+
+// RecordPluginOperationResult records the result of a plugin operation
+func RecordPluginOperationResult(operation string, result interface{}) {
+	var resultString string
+	switch r := result.(type) {
+	case plugin.GrantStatus:
+		resultString = string(r)
+	case plugin.RevokeStatus:
+		resultString = string(r)
+	case error:
+		resultString = "error"
+	default:
+		resultString = "unknown"
+	}
+	pluginOperationsTotal.WithLabelValues(operation, resultString).Inc()
 }
