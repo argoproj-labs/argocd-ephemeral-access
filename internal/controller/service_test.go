650c5fe55a888177a6d69b871c71afbc0848290a
Leonardo Luz Almeida <leoluz@users.noreply.github.com>
2025-04-28 11:46:32 -0400

    fix: Properly register plugin message updates in the history (#97)
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>

--- internal/controller/service_test.go
+++ internal/controller/service_test.go
@@ -9,6 +9,7 @@ import (
 	argocd "github.com/argoproj-labs/argocd-ephemeral-access/api/argoproj/v1alpha1"
 	api "github.com/argoproj-labs/argocd-ephemeral-access/api/ephemeral-access/v1alpha1"
 	"github.com/argoproj-labs/argocd-ephemeral-access/internal/controller"
+	"github.com/argoproj-labs/argocd-ephemeral-access/pkg/plugin"
 	"github.com/argoproj-labs/argocd-ephemeral-access/test/mocks"
 	"github.com/argoproj-labs/argocd-ephemeral-access/test/utils"
 	"github.com/stretchr/testify/assert"
@@ -84,6 +85,78 @@ func TestHandlePermission(t *testing.T) {
 			assert.Contains(t, err.Error(), expectedError.Error())
 			assert.NotNil(t, status, "status is nil")
 			assert.Equal(t, "", string(status))
+		})
+	})
+	t.Run("will handle plugins", func(t *testing.T) {
+		t.Run("will update the history with the latest plugin message", func(t *testing.T) {
+			// Given
+			resourceWriterMock := mocks.NewMockSubResourceWriter(t)
+			resourceWriterMock.EXPECT().Update(mock.Anything, mock.AnythingOfType("*v1alpha1.AccessRequest")).Return(nil)
+			k8sMock := mocks.NewMockK8sClient(t)
+			k8sMock.EXPECT().Status().Return(resourceWriterMock)
+			pluginMock := mocks.NewMockAccessRequester(t)
+			response1 := &plugin.GrantResponse{
+				Status:  plugin.GrantStatusPending,
+				Message: "message 1",
+			}
+			pluginMock.EXPECT().
+				GrantAccess(mock.AnythingOfType("*v1alpha1.AccessRequest"), mock.AnythingOfType("*v1alpha1.Application")).
+				Return(response1, nil).Once()
+			pendingMessage := "expected message"
+
+			response2 := &plugin.GrantResponse{
+				Status:  plugin.GrantStatusPending,
+				Message: pendingMessage,
+			}
+			pluginMock.EXPECT().
+				GrantAccess(mock.AnythingOfType("*v1alpha1.AccessRequest"), mock.AnythingOfType("*v1alpha1.Application")).
+				Return(response2, nil).Times(2)
+
+			approvedMessage := "approved"
+			response3 := &plugin.GrantResponse{
+				Status:  plugin.GrantStatusGranted,
+				Message: "approved",
+			}
+			pluginMock.EXPECT().
+				GrantAccess(mock.AnythingOfType("*v1alpha1.AccessRequest"), mock.AnythingOfType("*v1alpha1.Application")).
+				Return(response3, nil)
+
+			k8sMock.EXPECT().
+				Get(mock.Anything, mock.Anything, mock.AnythingOfType("*v1alpha1.AppProject")).
+				RunAndReturn(func(ctx context.Context, key types.NamespacedName, obj client.Object, opts ...client.GetOption) error {
+					obj = &argocd.AppProject{}
+					return nil
+				}).
+				Once()
+			k8sMock.EXPECT().
+				Patch(mock.Anything, mock.AnythingOfType("*v1alpha1.AppProject"), mock.Anything, mock.Anything).
+				Return(nil).
+				Once()
+
+			svc := controller.NewService(k8sMock, nil, pluginMock)
+			ar := utils.NewAccessRequest("test", "default", "someApp", "someAppNs", "someRole", "someRoleNs", "")
+			app := &argocd.Application{}
+			rt := &api.RoleTemplate{
+				Spec: api.RoleTemplateSpec{
+					Name:        "some-role",
+					Description: "some-role-description",
+					Policies:    []string{"some-policy"},
+				},
+			}
+
+			// When
+			svc.HandlePermission(context.Background(), ar, app, rt)
+			svc.HandlePermission(context.Background(), ar, app, rt)
+			svc.HandlePermission(context.Background(), ar, app, rt)
+			status, err := svc.HandlePermission(context.Background(), ar, app, rt)
+
+			// Then
+			assert.NoError(t, err)
+			assert.NotNil(t, status, "status is nil")
+			assert.Equal(t, api.GrantedStatus, status)
+			assert.Equal(t, pendingMessage, ar.GetLastStatusDetails(api.RequestedStatus))
+			assert.Equal(t, approvedMessage, ar.GetLastStatusDetails(api.GrantedStatus))
+			assert.Len(t, ar.Status.History, 4)
 		})
 	})
 }
