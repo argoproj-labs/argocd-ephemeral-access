2bd9b71ea3aaef4d1fa8a2dda3d800a42ac18eb4
Leonardo Luz Almeida <leoluz@users.noreply.github.com>
2025-06-05 16:04:17 -0400

    chore: Improve controller logs adding context fields (#112)
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>

--- internal/controller/accessrequest_controller.go
+++ internal/controller/accessrequest_controller.go
@@ -95,6 +95,14 @@ func (r *AccessRequestReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 		logger.Error(err, "Error retrieving AccessRequest from k8s")
 		return ctrl.Result{}, err
 	}
+	values := []interface{}{
+		"subject", ar.Spec.Subject.Username,
+		"role", ar.Spec.Role.FriendlyName,
+		"duration", ar.Spec.Duration.String(),
+		"application.name", ar.Spec.Application.Name,
+		"application.namespace", ar.Spec.Application.Namespace,
+	}
+	logger = logger.WithValues(values...)
 
 	// check if the object is being deleted and properly handle it
 	logger.Debug("Handling finalizer")
