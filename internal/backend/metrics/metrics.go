3641a239067de62a7daf5e89b54fceaf67b2c575
Leonardo Luz Almeida <leonardo_almeida@intuit.com>
2025-09-29 16:36:59 -0400

    feat: add status to api_requests_total metric
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>

--- internal/backend/metrics/metrics.go
+++ internal/backend/metrics/metrics.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -14,7 +15,7 @@ var (
 			Name: "api_requests_total",
 			Help: "Total number of API requests",
 		},
-		[]string{"method", "path"},
+		[]string{"method", "path", "status"},
 	)
 
 	apiRequestDuration = prometheus.NewHistogramVec(
@@ -38,18 +39,30 @@ func MetricsHandler() http.Handler {
 }
 
 // RecordAPIRequest records metrics for an API request
-func recordAPIRequest(method, path string, duration float64) {
-	apiRequestsTotal.WithLabelValues(method, path).Inc()
+func recordAPIRequest(method, path string, duration float64, statusCode int) {
+	apiRequestsTotal.WithLabelValues(method, path, strconv.Itoa(statusCode)).Inc()
 	apiRequestDuration.WithLabelValues(method, path).Observe(duration)
 }
 
 // MetricsMiddleware is a middleware function that records API request metrics.
 func MetricsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		wrapper := &responseWriterWrapper{ResponseWriter: w}
 		start := time.Now()
-		next.ServeHTTP(w, r)
+		next.ServeHTTP(wrapper, r)
 		// Get duration in milliseconds
 		duration := float64(time.Since(start).Milliseconds())
-		recordAPIRequest(r.Method, r.URL.Path, duration)
+
+		recordAPIRequest(r.Method, r.URL.Path, duration, wrapper.statusCode)
 	})
+}
+
+type responseWriterWrapper struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriterWrapper) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
