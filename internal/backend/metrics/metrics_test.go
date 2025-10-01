17df7c68113cf44d32359af9984453b8a5ef3ca7
Leonardo Luz Almeida <leoluz@users.noreply.github.com>
2025-10-01 09:37:16 -0400

    feat: add status to api_requests_total metric (#134)
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>

--- internal/backend/metrics/metrics_test.go
+++ internal/backend/metrics/metrics_test.go
@@ -7,17 +7,23 @@ import (
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/prometheus/common/expfmt"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRecordAPIRequest(t *testing.T) {
 	method := "GET"
 	path := "/accessrequests"
 	duration := 123.0
+	status := 200
 
-	recordAPIRequest(method, path, duration)
+	recordAPIRequest(method, path, duration, status)
 	assert.Equal(t, 1, testutil.CollectAndCount(apiRequestsTotal, "api_requests_total"))
 	assert.Equal(t, 1, testutil.CollectAndCount(apiRequestDuration, "api_request_duration_milliseconds"))
+	out, err := testutil.CollectAndFormat(apiRequestsTotal, expfmt.TypeTextPlain, "api_requests_total")
+	require.NoError(t, err)
+	assert.Contains(t, string(out), "status=\"200\"")
 }
 
 func TestMetricsMiddleware(t *testing.T) {
